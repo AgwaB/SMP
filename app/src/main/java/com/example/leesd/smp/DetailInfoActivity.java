10004f2a3fc655ac9e722104ed42506c16b3290f
KIMSJ <leesd556@gmail.com>
2018-05-07 15:31:07 +0900

    세부설명 image slider 추가

--- app/src/main/java/com/example/leesd/smp/DetailInfoActivity.java
+++ app/src/main/java/com/example/leesd/smp/DetailInfoActivity.java
@@ -5,11 +5,17 @@ import android.media.Rating;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.DefaultSliderView;
 import com.example.leesd.smp.DetailSearch.JsonDetail;
+import com.example.leesd.smp.DetailSearch.Photo;
 import com.example.leesd.smp.DetailSearch.Review;
 import com.example.leesd.smp.RetrofitCall.AsyncResponseMaps;
 import com.example.leesd.smp.RetrofitCall.DetailInfoNetworkCall;
@@ -19,6 +25,7 @@ import com.example.leesd.smp.googlemaps.Result;
 
 import org.w3c.dom.Text;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -30,9 +37,14 @@ import retrofit2.Response;
  */
 
 public class DetailInfoActivity extends AppCompatActivity implements AsyncResponseMaps {
+    private final String photoBaseUrl = "https://maps.googleapis.com/maps/api/place/photo?";
+    private SliderLayout sliderLayout; // image slider layout :  external library
+    private DefaultSliderView defaultSliderView;
+
     private ListView listView;
     private DetailListViewAdapter adapter;
     private TextView txt_title, txt_address;
+    private ImageButton btn_back;
     private RatingBar ratingBar;
     private Result jsonResult;
     private int position;
@@ -43,10 +55,17 @@ public class DetailInfoActivity extends AppCompatActivity implements AsyncRespon
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); // makes window fullscreen
         setContentView(R.layout.activity_detail);
+
+
+
+        sliderLayout = (SliderLayout)findViewById(R.id.slider);
 
         listView = (ListView)findViewById(R.id.detailListView);
         txt_title = (TextView)findViewById(R.id.IDtitle);
+        btn_back = (ImageButton)findViewById(R.id.backButton);
         //txt_address = (TextView)findViewById(R.id.IDadress);
         ratingBar = (RatingBar)findViewById(R.id.ratingBar);
 
@@ -64,6 +83,14 @@ public class DetailInfoActivity extends AppCompatActivity implements AsyncRespon
             ratingBar.setRating(Float.valueOf(Double.toString(jsonResult.getRating()))); // (float)의 명시적 형변환 안됨. double->string->float로 변환
         txt_title.setText(jsonResult.getName());
         //txt_address.setText(jsonResult.getVicinity());
+
+
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     public void getDetailData(String placeid, String reference){
@@ -91,6 +118,11 @@ public class DetailInfoActivity extends AppCompatActivity implements AsyncRespon
         n.execute(call);
     }
 
+    public void setPhotos(String photoreference, String maxheight, String maxwidth){
+        defaultSliderView = new DefaultSliderView(this);
+        defaultSliderView.image(photoBaseUrl + "key=" + getString(R.string.placesKey) + "&photoreference=" + photoreference + "&maxheight=" + maxheight + "&maxwidth=" + maxwidth );
+        sliderLayout.addSlider(defaultSliderView);
+    }
     @Override
     public void processFinish(Response<JsonMaps> response) {
 
@@ -101,13 +133,25 @@ public class DetailInfoActivity extends AppCompatActivity implements AsyncRespon
         if(response!=null)
             jsonDetail = response.body();
             ArrayList<Review> review = null;
+            ArrayList<Photo> photo = null;
+
         if(jsonDetail.getResult().getReviews()!=null) {
             review = (ArrayList<Review>) jsonDetail.getResult().getReviews();
 
             for (int i = 0; i < review.size(); i++)
                 adapter.addItem(review.get(i).getProfilePhotoUrl(), review.get(i).getRelativeTimeDescription(), review.get(i).getAuthorName(), review.get(i).getText(), review.get(i).getRating());
-
             adapter.notifyDataSetChanged();
         }
+        if(jsonDetail.getResult().getPhotos()!=null) {
+                photo = (ArrayList<Photo>) jsonDetail.getResult().getPhotos();
+            for (int i = 0; i < photo.size(); i++)
+                setPhotos(photo.get(i).getPhotoReference(), "300", "300");
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        sliderLayout.startAutoCycle(); // To prevent a memory leak on rotation.
+        super.onStop();
     }
 }
